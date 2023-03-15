836db6f525432abe8ca899121150a6613ee6f489
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-03-15 15:53:06 +0100

    Add image control

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -13,7 +13,11 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = Project::where('is_published', true)->with('type', 'technologies')->orderBy('updated_at', 'DESC')->get();
+        $projects = Project::where('is_published', true)->with('type', 'technologies')->orderBy('updated_at', 'DESC')->paginate(4);
+        foreach ($projects as $project) {
+            if ($project->image) $project->image = url('storage/' . $project->image);
+            else $project->image = 'https://media.istockphoto.com/id/1357365823/vector/default-image-icon-vector-missing-picture-page-for-website-design-or-mobile-app-no-photo.jpg?s=612x612&w=0&k=20&c=PM_optEhHBTZkuJQLlCjLz-v3zzxp-1mpNQZsdjrbns=';
+        }
         return response()->json($projects);
     }
 
